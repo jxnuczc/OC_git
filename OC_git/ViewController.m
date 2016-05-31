16accd85f20640f01f0fe1803458eb2563d375f0
jxnuczc <463159696@qq.com>
2016-05-31 09:39:49 +0800

    init

--- OC_git/ViewController.m
+++ OC_git/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //这是初始化创建
 }
 
 - (void)didReceiveMemoryWarning {
